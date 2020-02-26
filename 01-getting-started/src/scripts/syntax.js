91ebbe371f0bf2eb24fc37c6531568eecc74a84b
Sean <sean.mortimer@gmail.com>
2020-02-26 16:31:49 -0700

    added several more functions and their tests

--- 01-getting-started/src/scripts/syntax.js
+++ 01-getting-started/src/scripts/syntax.js
@@ -1,16 +1,4 @@
-// arrays
-    // add to the front
-    // add to the end
-    // update values
-// loops 
-    // for
-    // for/in
-    // while
-    // do while
-    // forEach (with array and function)
-// Objects / Dictionaries
-    // declare object
-    // lookup key to retrieve value
+// Emoji bank: 🦕 🐊 🐉 🦎 🐍 🦖 🌋
 
 const syntaxFunctions = {
 // define attributes / variables
@@ -37,9 +25,10 @@ const syntaxFunctions = {
     
     // array (adds a number equal to its index + 1 to the end of an array)
     array: (array) => {
-        array.push(array.length + 1);
-        // console.log("array is: " + array);
-        return array;
+        const newArray = [...array];
+        newArray.push(array.length);
+        // console.log("newArray is: " + array);
+        return newArray;
     },
 
     // dictionary / objects --Adds "animal: dog" to the object, because everything
@@ -65,10 +54,119 @@ const syntaxFunctions = {
 
 // functions
     // parameters
+    useParameters: (num1, num2) => {
+        return num1 + num2;
+    },
     
-    
-    // returns
+    // returns 
+    returns: (string) => {
+        return string + " is a string";
+    },
+
+// arrays
+    // add to the front the initial length of the array
+    addToFront: (array) => {
+        const newArray = [...array];
+        newArray.unshift(newArray.length);
+        return newArray;
+    },
+
+    // add to the end the initial length of the array
+    addToEnd: (array) => {
+        const newArray = [...array];
+        newArray.push(array.length);
+        return newArray; 
+    },
+
+    // update values (change first three values to zero)
+    updateArray: (array) => {
+        const newArray = [...array];
+        newArray[0] = 0;
+        newArray[1] = 0;
+        newArray[2] = 0;
+        return newArray;
+    },
+
+// loops 
+    // for
+    forLoop: (num) => {
+        let i; 
+        for (i = 0; i < num; i++) {
+        // console.log(i+1);
+        }
+    return "I have counted to " + i + 
+    ", I am a very good computer!"; 
+    },
+
+    // for/in
+    forIn: (names) => {
+        let text = "List of names: ";
+        let x;
+        
+        for (x in names) {
+            text += names[x] + ", "
+            // console.log(text);
+        }
+        return text;
+    },
+
+    // while: Write the alphabet up to a target number
+    aWhileLoop: (target) => {
+        let text = "";
+        let i = 0;
+
+        while (i < target - 1) {
+            text += String.fromCharCode(65 + i) + ", "
+            i++;
+        }
+        if (i != 0) {text += String.fromCharCode(65 + i)}
+        text += "!";
+        // console.log(text);
+        return text;
+    },
+
+    // do while: Write the alphabet up to a target number
+    // will always return at least "A!"
+    doWhileLoop: (target) => {
+        let text = "";
+        let i = 0;
+
+        do {
+            text += String.fromCharCode(65 + i)
+            if (i < target - 1) {text += ", ";};
+            i++;
+        }
+        while (i < target);
+        text += "!";
+        // console.log("return text: " + text);
+        return text;
+
+    },
+
+    // forEach (with array and function)
+    //Array
+    forEachArray: (array) => {
+        let newArray = [...array];
+
+        const makeExtinct = (item, index, array) => {
+            array[index] = item + "  🌋" ;
+            // console.log("makeExtinct is: " + array[index]);
+            };
+
+        newArray.forEach(makeExtinct);      
+        
+        // console.log("newArray is now: " + newArray);
+        return newArray;
+    }
+
+// Objects / Dictionaries
+    // declare object
+
+
+
+    // lookup key to retrieve value
 
 }
 
 export default syntaxFunctions;
+
--- 01-getting-started/src/scripts/syntax.test.js
+++ 01-getting-started/src/scripts/syntax.test.js
@@ -29,9 +29,9 @@ test('Does the boolean function work?', () => {
 });
 
 test('Does the array function work?', () => {
-    expect(syntaxFunctions.array([1,2])).toEqual([1,2,3]);
-    expect(syntaxFunctions.array([88,99,77])).toEqual([88,99,77,4]);
-    expect(syntaxFunctions.array(["a","b","c","d"])).toEqual(["a","b","c","d",5]);
+    expect(syntaxFunctions.array([1,2])).toEqual([1,2,2]);
+    expect(syntaxFunctions.array([88,99,77])).toEqual([88,99,77,3]);
+    expect(syntaxFunctions.array(["a","b","c","d"])).toEqual(["a","b","c","d",4]);
 });
 
 test('Does the object function work?', () => {
@@ -48,13 +48,84 @@ test('Does the undefined function work?', () => {
     expect(syntaxFunctions.undefinedCheck("a")).toBe(false);
     expect(syntaxFunctions.undefinedCheck(5)).toBe(false);
     expect(syntaxFunctions.undefinedCheck(null)).toBe(false);
-
-
 });
 
 test('Does the if / else sample function work?', () => {
     expect(syntaxFunctions.isItZero(0)).toBe(true);
     expect(syntaxFunctions.isItZero(1)).toBe(false);
     expect(syntaxFunctions.isItZero(-1)).toBe(false);
+});
 
+test('Does the parameter sample function work?', () => {
+    expect(syntaxFunctions.useParameters(0,1)).toBe(1);
+    expect(syntaxFunctions.useParameters(1,2)).toBe(3);
+    expect(syntaxFunctions.useParameters(-1,8)).toBe(7);
+});
+
+test('Does the returns sample function work?', () => {
+    expect(syntaxFunctions.returns("username")).toBe("username is a string");
+    expect(syntaxFunctions.returns("password")).toBe("password is a string");
+    expect(syntaxFunctions.returns(5)).toBe("5 is a string");
+});
+
+test('Does the Add to Front of Array function work?', () => {
+    expect(syntaxFunctions.addToFront([0,1,2])).toEqual([3,0,1,2]);
+    expect(syntaxFunctions.addToFront([0,1,2,3])).toEqual([4,0,1,2,3]);
+    expect(syntaxFunctions.addToFront(["a","b","c","d","e","f"]))
+        .toEqual([6,"a","b","c","d","e","f"]);
+});
+
+test('Does the Add to End of Array function work?', () => {
+    expect(syntaxFunctions.addToEnd([0,1,2])).toEqual([0,1,2,3]);
+    expect(syntaxFunctions.addToEnd([0,1,2,3,4,5])).toEqual([0,1,2,3,4,5,6]);
+    expect(syntaxFunctions.addToEnd(["a","b","c","d","e","f"]))
+     .toEqual(["a","b","c","d","e","f",6]);
+});   
+
+test('Does the Update Array Values function work?', () => {
+    expect(syntaxFunctions.updateArray([0,1,2])).toEqual([0,0,0]);
+    expect(syntaxFunctions.updateArray([true, false, true, true]))
+        .toEqual([0,0,0, true]);
+    expect(syntaxFunctions.updateArray(["a","b","c","d","e","f"]))
+        .toEqual([0,0,0,"d","e","f"]);
+    expect(syntaxFunctions.updateArray([5])).toEqual([0,0,0]);
+});   
+
+test('Does the For Loop function work?', () => {
+    expect(syntaxFunctions.forLoop(10))
+        .toBe("I have counted to 10, I am a very good computer!");
+    expect(syntaxFunctions.forLoop(500))
+        .toBe("I have counted to 500, I am a very good computer!");
+    expect(syntaxFunctions.forLoop(-5))
+        .toBe("I have counted to 0, I am a very good computer!");
+});   
+
+test('Does the For / In Loop function work?', () => {
+    expect(syntaxFunctions.forIn({name1: "Bill", name2: "Ted"}))
+        .toBe("List of names: Bill, Ted, ");
+    expect(syntaxFunctions.forIn({name1: "Daffy", name2: "Bugs"}))
+        .toBe("List of names: Daffy, Bugs, ");
+});   
+
+test('Does the While Loop function work?', () => {
+    expect(syntaxFunctions.aWhileLoop(3)).toBe("A, B, C!");
+    expect(syntaxFunctions.aWhileLoop(10))
+        .toBe("A, B, C, D, E, F, G, H, I, J!");
+    expect(syntaxFunctions.aWhileLoop(-5)).toBe("!");
+});   
+
+test('Does the While Loop function work?', () => {
+    expect(syntaxFunctions.doWhileLoop(3)).toBe("A, B, C!");
+    expect(syntaxFunctions.doWhileLoop(10))
+        .toBe("A, B, C, D, E, F, G, H, I, J!");
+    expect(syntaxFunctions.doWhileLoop(-5)).toBe("A!");
+
+});   
+
+test('Does the array function work?', () => {
+    expect(syntaxFunctions.forEachArray(["🦕", "🐉"]))
+        .toEqual(["🦕  🌋", "🐉  🌋"]);
+    expect(syntaxFunctions.forEachArray(["🦕", "🐊", "🐉", "🦎", "🦖"]))
+        .toEqual(["🦕  🌋", "🐊  🌋", "🐉  🌋", "🦎  🌋", "🦖  🌋"]);
+    
 });
