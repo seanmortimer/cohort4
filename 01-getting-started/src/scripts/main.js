274faad551954bad3e4129c8e00f81a950d4486d
Sean <sean.mortimer@gmail.com>
2020-02-28 15:55:57 -0700

    added more calculator functionality

--- 01-getting-started/src/scripts/main.js
+++ 01-getting-started/src/scripts/main.js
@@ -5,6 +5,7 @@ import functions from './functions.js';
 // Add the event listeners
 // 
 const numbers = [];
+let operator;
 
 
 idNumber.addEventListener('change', (() => {
@@ -12,13 +13,77 @@ idNumber.addEventListener('change', (() => {
 }));
 
 
-idCalcNum1.addEventListener('change', (() => {
+
+document.addEventListener("keyup", ((event) => {
+    var keyPressed = event.key;
+
+    // console.log("the key pressed was: " + keyPressed)
+    
+    if (keyPressed === "+") {
+        console.log("The operator is + ");
+        numbers.push(Number(idCalcInput.value));
+
+        idOperator.textContent = "  +  ";
+        idCalcInput.value = "";
+
+        operator = keyPressed;
+    }
+
+    if (keyPressed === "-") {
+        console.log("The operator is - "); 
+        numbers.push(Number(idCalcInput.value));
+
+        idOperator.textContent = "  -  ";
+        idCalcInput.value = "";
+
+        operator = keyPressed;
+
+    }
+
+    if (keyPressed === "*") {
+        console.log("The operator is × "); 
+        numbers.push(Number(idCalcInput.value));
+        idOperator.textContent = "  ×  ";
+        idCalcInput.value = "";
+
+        operator = keyPressed;
+
+    }
+
+    if (keyPressed === "/") {
+    console.log("The operator is ÷ "); 
+        numbers.push(Number(idCalcInput.value));
+        idOperator.textContent = "  ÷  ";
+        idCalcInput.value = "";
+
+        operator = keyPressed;
+    }
+
+    if (keyPressed === "Enter") {
+        console.log("Enter was pressed"); 
+        numbers.push(Number(idCalcInput.value));
+        idCalcInput.value = "";
+    
+            operator = keyPressed;
+        }
+
+}));
+
+
+idCalcInput.addEventListener('change', (() => {
        
-    numbers.push(Number(idCalcNum1.value));
-    // console.log("input type: " + typeof idCalcNum1.value);
-    console.log("numbers typeof 0: " + typeof numbers[0]);
+    numbers.push(Number(idCalcInput.value));
+    idCalcInput.value = "";
+    // console.log("input type: " + typeof idCalcResult.value);
+    idCalcResult.textContent = numbers[numbers.length - 1];
+
     console.log("numbers array: " + numbers);    
-    if (numbers.length > 1)
-    idCalcResult.textContent = functions.add(numbers);
+    if (numbers.length > 1) {
+    idCalcResult.textContent = numbers[numbers.length - 2];
+    idCalcResult.textContent += " and " + numbers[numbers.length - 1];
+    let result = functions.add(numbers)
+    numbers.push(result);
+    idCalcResult.textContent += " = " + result;
+    }
 }));
 
