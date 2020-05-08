672b04ca0346e869eb117da0b7e01cf881f6f787
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-08 13:46:14 -0600

    Add conditional renderings to react 01

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComp from './components/MyComponent'
+import EvenComp from './components/EvenComponent'
+import OddComp from './components/OddComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor() {
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "TBD"
+    };
+  }
+
+  onPushMe = () => {
+    this.counter++;
+    console.log("You pushed me", this.counter);
+    this.setState({ myState: 'now: ' + this.counter })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>I am in charge of this application and my name is Sean {this.state.myState}</h1>
+          <button onClick={this.onPushMe}>
+            Push Me
+          </button>
+          <MyComp whatToSay="What Ever!" counter={this.counter}  />
+          <EvenComp counter={this.counter} />
+          <OddComp counter={this.counter} />
+          <p>
+            A very useful app by Sean
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
