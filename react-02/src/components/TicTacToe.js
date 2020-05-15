abef78678bec331292bd38212cc0d7c751c18a61
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-15 00:19:34 -0600

    Complete Tic Tac Toe game

--- react-02/src/components/TicTacToe.js
+++ react-02/src/components/TicTacToe.js
@@ -11,7 +11,6 @@ function Square(props) {
   );
 }
 
-
 class Board extends React.Component {
   renderSquare(i) {
     return (
@@ -52,12 +51,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -68,14 +68,36 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo = (step) => {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+  
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move ' + move :
+        'Reset game';
+        return (
+        <li key={move}> 
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+        );
+    });
+
     let status;
     if (winner) {
       status = `The winner is: Player ${winner}!`;
@@ -91,13 +113,14 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="clStatus">{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
+
 
 function calculateWinner(squares) {
   const lines = [
