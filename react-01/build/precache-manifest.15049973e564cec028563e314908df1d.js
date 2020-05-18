bf7de0296e46d78a4f9d66912eb8e5837fc61dc2
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-17 22:59:54 -0600

    Play with React builds

---
+++ react-01/build/precache-manifest.15049973e564cec028563e314908df1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c2bcd35aacf6793518ea0e312fd7ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52390de24d9332f730f2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d10c13724cfddaf591c",
+    "url": "/static/js/2.6cedca66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cedca66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52390de24d9332f730f2",
+    "url": "/static/js/main.e6fde8a4.chunk.js"
+  },
+  {
+    "revision": "c90d2048a8f3232d7647",
+    "url": "/static/js/runtime-main.241a544a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
