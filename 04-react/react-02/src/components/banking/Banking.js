fb2959019afc9b4b40caafb2db52cc0e9d1ce417
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-22 22:09:11 -0600

    Start migrating banking app to React

---
+++ 04-react/react-02/src/components/banking/Banking.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Banking extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <main>
+                <Title></Title>
+                {/* <DepositWithdraw></DepositWithdraw> */}
+                {/* <ActCreator></ActCreator> */}
+                {/* <Messages></Messages> */}
+                {/* <ActContainer></ActContainer> // This might not be neccessary? */}
+            </main>
+        );
+    }
+}
+
+export default Banking;
