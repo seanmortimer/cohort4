bbdb3f08d828df26ee7f8e1c47b34c15dd176aec
Sean Mortimer <sean.mortimer@gmail.com>
2020-06-08 17:35:48 -0600

    Lifo component testing

--- 04-react/react-02/src/components/lists/LifoComponent.test.js
+++ 04-react/react-02/src/components/lists/LifoComponent.test.js
@@ -28,27 +28,3 @@ test('Lifo stack renders with data', () => {
   expect(screen.getByText(/dog, \$40/i)).toBeInTheDocument();
 });
 
-
-// test.only('fetching elephants', async () => {
-//   // const data = await fetch(url);
-
-//   const geoDb = 'http://geodb-free-service.wirefreethought.com/v1/geo/cities';
-
-
-//   // const data = await fetch(`${geoDb}?offset=${10}`).then((response) => {
-//   //   console.log('response.status :>> ', response.status);
-//   //   return response.json()}).then((id) => {
-//   //   console.log('id :>> ', id);
-//   //   return id;
-//   //   // console.log('response.status :>> ', response.status);
-//   // });
-
-//   const url = 'https://elephant-api.herokuapp.com/elephants/random';
-
-//   const response = await fetch(url);
-//   console.log('status :>> ', response.status);
-//   console.log('status :>> ', response.body);
-//   const data = await response.json();
-//   console.log('data :>> ', data);
-
-// });
