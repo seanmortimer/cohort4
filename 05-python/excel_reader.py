a88e8f13a78bed598bb2b6ac0e196656fc880573
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-15 16:31:49 -0600

    Add invoice printing functions

--- 05-python/excel_reader.py
+++ 05-python/excel_reader.py
@@ -1,6 +1,11 @@
 from openpyxl import load_workbook
 print()
 
+# ADD ONS: Data validation, 
+# make into class?
+# make print width more adjustable
+
+
 def excelToDict(file):
     wb = load_workbook(file)
     wb_dict = {}  
@@ -34,3 +39,82 @@ def alternateWay(file):
            i += 1
 
     return wb_dict
+
+
+def buildInvoice(file, invoice_no):
+    wb_dict = excelToDict(file)
+    invoice = {"invoice_no": invoice_no}
+
+    custID = wb_dict["Invoices"][invoice_no]["customer_id"]
+    invoice["customer"] = wb_dict["Customers"][custID]
+
+    # invoice["invoice_no"] = invoice_no
+    invoice["date"] = wb_dict["Invoices"][invoice_no]["date"]
+    invoice["payment"] = wb_dict["Invoices"][invoice_no]["payment"]
+    items = [value for (line, value) in wb_dict["Line_Items"].items() if value["invoice_no"] == invoice_no]
+    invoice["line_items"] = {}
+    invoice["total"] = 0
+
+    for item in items:
+        product = wb_dict["Products"][item["product_id"]]
+        unit_price = product["price"]
+        qty = item["quantity"]
+        price = unit_price * qty
+        line = len(invoice["line_items"]) + 1
+
+        invoice["line_items"][line] = {
+            "product_id": item["product_id"],
+            "name": product["product_name"],
+            "unit_price": unit_price,
+            "qty": int(qty),
+            "price": price
+        }
+        invoice["total"] += price
+
+    return invoice
+
+
+def printInvoice(file, invoice_no):
+    print_width = 80
+    invoice = buildInvoice(file, invoice_no)
+
+
+    i_str = f"Invoice No: {invoice_no}"
+    date = f"Date: {invoice['date']}"
+    name = f"{invoice['customer']['first_name']} {invoice['customer']['last_name']}"
+    addr = f"{invoice['customer']['address']}"
+    phone = invoice["customer"]["phone"]
+
+    
+    # header
+    print("*".ljust(print_width, '*'))
+    print("\n" + name.ljust(print_width - len(i_str)) + i_str)
+    print(addr + date.rjust(print_width - len(addr)))
+    print(f"{phone}\n\n")
+
+    # body
+    print("-".ljust(print_width, '-'))
+    print("Item No".ljust(18), "Name".ljust(25), "Unit Price".ljust(14), "Quantity".ljust(13), "Price")
+    print("-".ljust(print_width, '-'))
+    
+    for line in invoice['line_items'].values():
+        print(
+            f"  {int(line['product_id'])}".ljust(10), 
+            f"{line['name']}"[0:60].ljust(35),
+            f"${line['unit_price']:.2f}", 
+            f"{line['qty']}".rjust(12), 
+            f"$ {line['price']:.2f}".rjust(12))
+    
+    print("-".ljust(print_width, '-'))
+    print(f"$ {invoice['total']:.2f}".rjust(print_width - 1))
+
+    # footer
+    print()
+    print("Thank you for choosing Billy's Grocery!".center(print_width))
+    print("*".ljust(print_width, '*'))
+
+    return invoice['total']
+
+
+# printInvoice('./data/billy_test_data.xlsx', 1)
+# printInvoice('./data/billy_accounting.xlsx', 2)
--- 05-python/excel_reader_test.py
+++ 05-python/excel_reader_test.py
@@ -2,6 +2,7 @@ import pytest
 from excel_reader import *
 
 test_file = './data/billy_test_data.xlsx'
+data_file = './data/billy_accounting.xlsx'
   
 def test_loadingDatatoDictionary():
     result = excelToDict(test_file)
@@ -41,4 +42,39 @@ def test_alternate():
 
     assert result["Line_Items"][1]["line_id"] == 1 
     assert result["Line_Items"][5]["line_id"] == 5 
-    assert result["Line_Items"][5]["quantity"] == 10 
+    assert result["Line_Items"][5]["quantity"] == 10
+
+def test_buildInvoice_customer():
+    result = buildInvoice(test_file, 1)
+    assert result["customer"]["first_name"] == "Adam"
+
+def test_buildInvoice_invoice():
+    invoice = buildInvoice(test_file, 1)
+    assert invoice["invoice_no"] == 1
+    assert invoice["date"] == "2020-01-01"
+    assert invoice["total"] == 6
+    assert invoice["payment"] == "visa"
+
+def test_buildInvoice_lineItems():
+    items = buildInvoice(test_file, 1)["line_items"]
+    assert len(items) == 3
+    assert items[1]["name"] == "Apple"
+    assert items[3]["price"] == 3
+    
+def test_buildSecondInvoice():
+    invoice = buildInvoice(test_file, 2)
+    assert invoice["invoice_no"] == 2
+    assert invoice["date"] == "2020-01-02"
+    assert invoice["payment"] == "amex"
+    assert len(invoice["line_items"]) == 2
+    assert invoice["total"] == 54
+
+def test_invoicePrint():
+    assert printInvoice(test_file, 1) == 6.00 
+    assert printInvoice(test_file, 2) == 54.00
+
+
+def test_invoicePrintData():
+    assert printInvoice(data_file, 1) == 1682.01
+    assert printInvoice(data_file, 2) == 11019.22
+    assert printInvoice(data_file, 25) == 16543.96
