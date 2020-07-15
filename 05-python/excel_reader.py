69eff6c4e6011c8040094ed2b439571383a0c5f8
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-15 01:04:06 -0600

    Clean up excel reader code

--- 05-python/excel_reader.py
+++ 05-python/excel_reader.py
@@ -1,19 +1,12 @@
 from openpyxl import load_workbook
 print()
 
-def load_excelbook(file):
+def excelToDict(file):
     wb = load_workbook(file)
     wb_dict = {}  
 
-    # for sheet_name in wb.sheetnames:
-        # wb_dict[sheet_name] = {} 
-    # print(wb_dict)
-
     for sheet in wb.worksheets:
-        # print('sheet:', sheet.title)
-    #     # print(sheet)
         wb_dict[sheet.title] = {}
-
 
         for row in sheet.iter_rows(min_row=2, min_col=1):
             id = row[0].value
@@ -21,13 +14,23 @@ def load_excelbook(file):
             for cell in row:
                 label = sheet.cell(row=1, column=cell.column).value
                 value = cell.value
-                # print(label)
-                # print(value)
                 wb_dict[sheet.title][id][label] = value
-                # print('wb_dict[sheet.title]:', wb_dict[sheet.title])
-    #         #     col = 0
-    #         #     for cell in row:
-    #         #         wb_dict[sheet][i][wb[sheet][2][col].value] = cell.value
-    #         #         col += 1
-    #         # i += 1
-    return(wb_dict)
+
+    return wb_dict
+
+
+def alternateWay(file):
+    wb = load_workbook(file)
+    wb_dict = {}  
+
+    for sheet in wb.worksheets:
+        wb_dict[sheet.title] = {}
+        i = 1
+        for row in sheet.values:
+           if i == 1:
+               labels = row 
+           else:
+               wb_dict[sheet.title][row[0]] = dict(zip(labels, row))
+           i += 1
+
+    return wb_dict
--- 05-python/excel_reader_test.py
+++ 05-python/excel_reader_test.py
@@ -4,7 +4,8 @@ from excel_reader import *
 test_file = './data/billy_test_data.xlsx'
   
 def test_loadingDatatoDictionary():
-    result = load_excelbook(test_file)
+    result = excelToDict(test_file)
+
     assert result["Customers"][1]["customer_id"] == 1 
     assert result["Customers"][1]["first_name"] == "Adam" 
     assert result["Customers"][1]["last_name"] == "West" 
@@ -15,6 +16,26 @@ def test_loadingDatatoDictionary():
     assert result["Invoices"][1]["date"] == "2020-01-01"
     assert result["Invoices"][1]["payment"] == "visa"
     
+    assert result["Products"][1]["product_name"] == "Apple"
+    assert result["Products"][5]["product_name"] == "Elderberry"
+
+    assert result["Line_Items"][1]["line_id"] == 1 
+    assert result["Line_Items"][5]["line_id"] == 5 
+    assert result["Line_Items"][5]["quantity"] == 10 
+
+def test_alternate():
+    result = alternateWay(test_file)
+
+    assert result["Customers"][1]["customer_id"] == 1 
+    assert result["Customers"][1]["first_name"] == "Adam" 
+    assert result["Customers"][1]["last_name"] == "West" 
+    assert result["Customers"][1]["address"] == "123 Wayne Manor" 
+
+    assert result["Invoices"][1]["invoice_no"] == 1
+    assert result["Invoices"][1]["customer_id"] == 1
+    assert result["Invoices"][1]["date"] == "2020-01-01"
+    assert result["Invoices"][1]["payment"] == "visa"
+
     assert result["Products"][1]["product_name"] == "Apple"
     assert result["Products"][5]["product_name"] == "Elderberry"
 
